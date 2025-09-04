f4f0525adc7edd769d4c4760a84768e5840cd3dd
Patrick Collins <dev@pecollins.com>
2025-09-03 20:29:47 -0500

    initial commit

---
+++ FinanceTracker/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FinanceTracker")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FinanceTracker.Pages")]
